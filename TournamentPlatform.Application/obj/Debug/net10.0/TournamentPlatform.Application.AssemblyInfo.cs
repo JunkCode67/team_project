67e4773cd8775b7cd4e60e51620c31a1f76fa28b
h1n322 <tyvanukmax@gmail.com>
2026-03-18 22:30:08 +0200

    Add TeamMember logic: registration, adding members and fetching team composition

--- TournamentPlatform.Application/obj/Debug/net10.0/TournamentPlatform.Application.AssemblyInfo.cs
+++ TournamentPlatform.Application/obj/Debug/net10.0/TournamentPlatform.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TournamentPlatform.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+409d0c87ad720035da121beb94abb268f2a9a87b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TournamentPlatform.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TournamentPlatform.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
